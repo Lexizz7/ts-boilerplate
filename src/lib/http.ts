a21bb2e671ce2aea1ffb5dfe454d93ba7ae4576f
Lexizz7 <lexizz777@proton.me>
2026-08-01 04:03:51 -0300

    Downgrade undici to version 7.18.2,

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,4 +1,10 @@
 import ky, { type Options } from 'ky';
+import { fetch, Headers, Request, Response } from 'undici';
+
+globalThis.fetch = fetch;
+globalThis.Request = Request;
+globalThis.Response = Response;
+globalThis.Headers = Headers;
 
 export function httpClient(defaultOptions?: Options) {
   const client = ky.create(defaultOptions);
